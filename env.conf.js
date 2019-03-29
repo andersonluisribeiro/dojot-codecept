9179de8f2b74b7d16ab40a9187670d860f4c6841
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-03-29 14:46:56 -0300

    updating mqtt host

--- env.conf.js
+++ env.conf.js
@@ -1,6 +1,6 @@
 let env = {
     dojot_host: process.env.DOJOT_HOST || 'http://10.202.21.25:8000',
-    mqtt_host: process.env.DOJOT_HOST || 'http://10.202.21.25'
+    mqtt_host: process.env.MQTT_HOST || 'http://10.202.21.25'
 };
 
 module.exports = env;
