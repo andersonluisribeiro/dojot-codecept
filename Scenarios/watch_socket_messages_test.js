78edf1002f81a263a34f22df20897f1ebf8c5765
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-03-29 15:58:27 -0300

    refactoring mqtt test

--- Scenarios/watch_socket_messages_test.js
+++ Scenarios/watch_socket_messages_test.js
@@ -1,54 +1,37 @@
-var mqtt = require('async-mqtt')
-var request = require('sync-request');
-var env = require('../env.conf');
-
 Feature('Watch socket messages');
 
 Before(login => {
     login('admin');
 });
 
-Scenario('Watching a simple message', async (I) => {
-    let jwt = await I.executeScript(() => { return localStorage.getItem('jwt')});
+Scenario('Watching a simple message', async (I, Device) => {
+   let template = await I.postJSON('template', {
+        label: "String Template",
+        attrs: [
+            {
+            label: "text",
+            type: "dynamic",
+            value_type: "string"
+            }
+        ]
+    });
 
-    let templateCreation = request('POST', `${env.dojot_host}/template`, {
-        headers: {
-            Authorization: `Bearer ${jwt}`
-        },
-        json: {
-            label: "String Template",
-            attrs: [
-                {
-                label: "text",
-                type: "dynamic",
-                value_type: "string"
-                }
-            ]
-        }
+    let templateId = template['template']['id'];
+
+    let device = await I.postJSON('device', {
+        "templates": [
+            templateId
+        ],
+        "label": "String device"
     });
     
-    let templateId = JSON.parse(templateCreation.getBody('utf8'))['template']['id'];
-
-    let deviceCreation = request('POST', `${env.dojot_host}/device`, {
-        headers: {
-            Authorization: `Bearer ${jwt}`
-        },
-        json: {
-            "templates": [
-                templateId
-            ],
-            "label": "String device"
-          }
-    });
+    let deviceId = device['devices'][0]['id'];
 
     I.refreshPage();
-    
-    let deviceId = JSON.parse(deviceCreation.getBody('utf8'))['devices'][0]['id'];
 
-    let client  = mqtt.connect(env.mqtt_host);
-    await client.publish(`/admin/${deviceId}/attrs`, '{"text": "my string"}');
-    await client.end();
-    I.click(locate('a').withAttr({ href: `#/device/id/${deviceId}/detail` }));
-    I.click(locate('div').withAttr({ title: 'text' }));
-    I.see('my string');
+    await I.sendMQTTMessage(deviceId, '{"text": "my string"}');
+    Device.editDevice(deviceId);
+    Device.selectAttr('text');
+    Device.shouldSeeMessage('my string');
+
 });
